c6bf08118a1b6ee3a0607d039d06679325fd7090
Michael Archibald <micharch54@gmail.com>
2012-04-12 09:43:40 -0600

    tag choice now working

--- src/commands/insertLineBreak.js
+++ src/commands/insertLineBreak.js
@@ -1,10 +1,22 @@
 (function(wysihtml5) {
   var undef,
-      LINE_BREAK = "<p>" + (wysihtml5.browser.needsSpaceAfterLineBreak() ? " " : "");
+      LINE_BREAK = "<br>" + (wysihtml5.browser.needsSpaceAfterLineBreak() ? " " : "");
 
   wysihtml5.commands.insertLineBreak = {
     exec: function(composer, command) {
-      wysihtml5.commands.formatBlock.exec(composer, "formatBlock", "P");
+      if (composer.config.breakElement === null){
+        if (composer.commands.support(command)) {
+          composer.doc.execCommand(command, false, null);
+          if (!wysihtml5.browser.autoScrollsToCaret()) {
+            composer.selection.scrollIntoView();
+          }
+        } else {
+          composer.commands.exec("insertHTML", LINE_BREAK);
+        }
+      }else{
+        wysihtml5.commands.formatBlock.exec(composer, "formatBlock", composer.config.breakElement);
+        composer.commands.exec("insertHTML", LINE_BREAK)
+      }
     },
 
     state: function() {
